743ec065df3efccb8e239ac46984055fb13e88d3
filippennsater <filip.pennsater@hotmail.com>
2024-09-04 21:20:55 +0200

    removed old servicepictures with a temp one, hosted the project and added descriptions of the services

--- src/pages/contact/ContactSection.jsx
+++ src/pages/contact/ContactSection.jsx
@@ -25,14 +25,14 @@ function ContactSection() {
                     <div className='h30'>Adress</div>
                     <div className='p30'>
                         Ateljé 9<br />
-                        Make-Believe Lane 00<br />
-                        000 00 LUND
+                        Steffensvägen 5 A<br />
+                        236 38 Höllviken
                     </div>
                     
                 </div>
                 <div className='right-container'>
                     <div className="map-container" dangerouslySetInnerHTML={{
-                        __html: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.9993771180305!2d13.197386428504018!3d55.70639341291944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465397cf7560906d%3A0x9de6e629c239cde3!2sBarnahus%20Lund!5e0!3m2!1ssv!2sse!4v1719426937163!5m2!1ssv!2sse" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+                        __html: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2264.9452090668824!2d12.940717277726081!3d55.41130721824148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46530c68b470523f%3A0x936c973aa05e1af8!2sSteffensv%C3%A4gen%205A%2C%20236%2038%20H%C3%B6llviken!5e0!3m2!1ssv!2sse!4v1725477200299!5m2!1ssv!2sse" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
                     }} />
                 </div>
             </div>
