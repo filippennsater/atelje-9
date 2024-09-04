4dbaac3e42054f10c9d5f7b94957bb674a83f445
filippennsater <filip.pennsater@hotmail.com>
2024-09-04 21:42:18 +0200

    fixed the about-text and the cv

--- src/pages/about/AboutSection.jsx
+++ src/pages/about/AboutSection.jsx
@@ -13,10 +13,10 @@ function AboutSection() {
                     <div className='header-container'>
                         {/*<div className='about-header'>OM</div>*/}
                     </div>
-                    <div className='about-text'>Efter att ha arbetat inom företagshälsovård i över 20 år och sett det ökande behovet av individanpassad behandling, kände jag en stark drivkraft att starta en egen klinik för att kunna erbjuda skräddarsydd kognitiv beteendeterapi och familjeterapi.<br />Mitt mål är att skapa en trygg och stödjande miljö där klienter kan få den hjälp de behöver för att hantera livets utmaningar.</div>
+                    <div className='about-text'>Efter många års erfarenhet och omfattande utbildning inom organisationspsykologi, ledarskap och stresshantering har jag specialiserat mig på att stödja individer och grupper i att nå sin fulla potential.<br/>Genom min bakgrund som stress- och ledarskapsterapeut har jag utvecklat en djup förståelse för hur neuroledarskap och personlighetsanalyser kan förbättra arbetsmiljön och effektiviteten inom organisationer. Jag är licensierad att använda flera psykologiska testverktyg, </div>
                 </div>
                 <div className='info-card2'>
-                    <div className='about-text'>Jag har över 20 års erfarenhet av behandling och samtal, samt omfattande erfarenhet av arbetsplatsrelaterad problematik inom företagshälsovård. <br /><br />Jag arbetar med evidensbaserad kognitiv beteendeterapi (KBT) och familjeterapi (IBCT), samt med utredning och bedömning av konflikter, svåra samtal, kränkningsanmälningar och arbetsförmågebedömningar.</div>
+                    <div className='about-text'>inklusive Myers-Briggs och Predictive Index, vilket gör det möjligt för mig att skräddarsy mina insatser för varje klient. Min erfarenhet som coach och yogalärare kompletterar min professionella kompetens och ger mig unika verktyg för att arbeta med både mental och fysisk hälsa. Jag brinner för att skapa hållbara förändringar och att hjälpa mina klienter att navigera genom arbetslivets utmaningar med ökad självinsikt och effektivitet.</div>
                 </div>
                 <div className='info-card3'>
                 </div>
@@ -25,10 +25,17 @@ function AboutSection() {
                         <div className='about-header'>CV</div>
                     </div>
                     <ul>
-                        <li className='about-text'>KBT-steg 1 Psykologpartners</li>
-                        <li className='about-text'>IBCT Psykologpartners</li>
-                        <li className='about-text'>ICF- Karolinska institutet, arbetsförmågebedömning</li>
-                        <li className='about-text'>Medicinskt ledningsansvar, Sveriges företagshälsovårds förening</li>
+                        <li className='about-text2'>UL - Utvecklande ledare Handledarutbildning Försvarshögskolan</li>
+                        <li className='about-text2'>UL-Utvecklande ledare grundkurs Försvarshögskolan</li>
+                        <li className='about-text2'>IBCT Psykologpartners</li>
+                        <li className='about-text2'>Yoga & Meditationslärare</li>
+                        <li className='about-text2'>UL - Utvecklande ledarskap Försvarshögskolan</li>
+                        <li className='about-text2'>Licensierad användare av psykologiskt test instrumentet, rekryteringsverktyg, Perspektiv, P A T Management AB Lund</li>
+                        <li className='about-text2'>Licensierad att använda Myers-Briggs personlighets Indicator Assessio International AB</li>
+                        <li className='about-text2'>Certifierad SMC-konsult Stress- och Arbetsmiljö-Profiler Profdoc Work AB</li>
+                        <li className='about-text2'>Certifierad användare av Förändringens Fyra Rum</li>
+                        <li className='about-text2'>PI Predictive Index testverktyg grupp/individ</li>
+                        <li className='about-text2'>Coach individ/grupp CoachCompanion Malmö</li>
                     </ul>
                 </div>
 
